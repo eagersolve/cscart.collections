36ecb5f831b0ff6c44b63f0f4f73ab0f98ce23ec
eagersolve <ekstaz1993@yandex.ru>
2022-04-12 18:32:15 +0400

    added .gitignore and cash

---
+++ var/cache/templates/bright_theme/d458b5de69b0664ff35a5011db7d0dc1c08fd343.backend.bp-modes__item--text.svg.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.21, created on 2022-04-12 17:19:05
+         compiled from "C:\Users\igorekeek\htdocs\cscart.collections\design\backend\templates\components\bottom_panel\icons\bp-modes__item--text.svg" */ ?>
+<?php /*%%SmartyHeaderCode:201530748462558a596df7b8-00379951%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'd458b5de69b0664ff35a5011db7d0dc1c08fd343' => 
+    array (
+      0 => 'C:\\Users\\igorekeek\\htdocs\\cscart.collections\\design\\backend\\templates\\components\\bottom_panel\\icons\\bp-modes__item--text.svg',
+      1 => 1643359490,
+      2 => 'backend',
+    ),
+  ),
+  'nocache_hash' => '201530748462558a596df7b8-00379951',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21',
+  'unifunc' => 'content_62558a596f7861_97243614',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_62558a596f7861_97243614')) {function content_62558a596f7861_97243614($_smarty_tpl) {?><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+    class="bp-modes__item-icon">
+    <path
+        d="M24.998 8.495A.502.502 0 0 0 24.495 8H8.503A.502.502 0 0 0 8 8.5v3.004c0 .276.225.5.503.5h1.637a.503.503 0 0 0 .393-.188l.855-1.813h3.603v13.496c0 .277.225.501.503.501h2.013a.502.502 0 0 0 .503-.5V10.002h4.098l.86 1.814c.095.119.24.187.392.187h1.137A.505.505 0 0 0 25 11.5l-.002-3.006z" />
+</svg><?php }} ?>
